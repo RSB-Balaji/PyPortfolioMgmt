cf4fc7a480ea47aeeca3f58e0a585da3d894e403
RSB-Balaji <rsbbalaji19@gmail.com>
2019-09-18 10:39:51 +0530

    defined fama french class

--- famafrench.py
+++ famafrench.py
@@ -1,3 +1,8 @@
+import pandas_datareader.data as data
+from pandas_datareader.famafrench import get_available_datasets
+
+import statsmodels.api as sm
+
 class FamaFrench:
     """
     FamaFrench class is implementation of the Fama-French three factor model.
@@ -5,27 +10,45 @@ class FamaFrench:
     def __init__(self, portfolioSet):
         self.portSet = portfolioSet
 
-    def getThreeFactorList(self ):
+    def printThreeFactorDataList(self ):
         """
         Obtains the list of datasets from Kenneth French data library related 
         to the 3 factor model.
+        http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/data_library.html
+        (Kenneth French data library)
         """
+        ffData = get_available_datasets()
+        self.ff3DataList = [dataset for dataset in ffData if '3_Factors' in dataset]
+        for i in range(0, len(self.ff3DataList)):
+            print(i,'.',self.ff3DataList[i])
 
     def getThreeFactorData(self, datasetIdx):
         """
-        Obtains the datasets metioned from the Kenneth French library.
-
+        Obtains the given dataset (datasetIdx) from the Kenneth French library.
         Parameters:
         -----------
-        datasetIdx (int) :  index of datasets obtained from getThreeFactorList.
+        datasetIdx (int) :  index of datasets obtained from getThreeFactorDataList().
         """
-    
-    def fitModel(self ):
+        df3Factor = data.DataReader(self.ff3DataList[datasetIdx], 'famafrench', self.portSet.startDate, self.portSet.endDate)
+        self.ff3FactorData = df3Factor[0]
+        self.ff3FactorData.index = self.ff3FactorData.index.astype('datetime64[ns]')
+        print(self.ff3FactorData.head())
+
+    def fitModel(self , ticker):
         """
-        Fits the Three Factor model to given portfolioSet.
+        Fits the Three Factor model to given ticker from the portfolioSet.
         """
-    
-    def printSummary(self ):
-        """
-        Prints the summary of fitted model.
-        """
+        dropIdx = set(self.ff3FactorData.index).difference(set(self.portSet.dReturns.index.intersection(self.ff3FactorData.index)))
+        self.ff3FactorData.drop(index =dropIdx, inplace =True)
+
+        xRet = (self.portSet.dReturns[ticker]*100 - self.ff3FactorData['RF'])
+
+        Y = xRet
+        X = self.ff3FactorData.iloc[ :, 0:3]
+        X = sm.add_constant(X)
+
+        self.model = sm.OLS(Y,X)
+        result = self.model.fit()
+        print(result.summary())
+
+        return result
--- test.py
+++ test.py
@@ -3,16 +3,24 @@ from datetime import datetime as dt
 from portfolio import PortfolioSet
 from portfolio import Portfolio
 from capm import Capm
+from famafrench import FamaFrench
 
 start = dt(2018,1,1)
 end = dt(2018,12,31)
 
+#Portfolio testing 
 p1 = PortfolioSet(["JNJ","PG","V"])
-
 p1.getDailyReturns(start, end)
 #p1.plotReturns()
 
-c = Capm(p1)
-opt = c.calcOptPortfolio(0.004,1000)
+# CAPM testing 
+#c = Capm(p1)
+#opt = c.calcOptPortfolio(0.004,1000)
 #c.plotCAL()
-c.plotSML("JNJ")
+#c.plotSML("JNJ")
+
+#FamaFrench testing
+f = FamaFrench(p1)
+f.printThreeFactorDataList()
+f.getThreeFactorData(11)
+f.fitModel("PG")
