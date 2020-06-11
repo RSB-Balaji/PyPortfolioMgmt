824cd1e9d063c1958d2d32eca8c1bbe378993b62
RSB <44422500+RSB-Balaji@users.noreply.github.com>
2020-06-11 12:36:51 +0530

    setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="pyportmgt",
+	name="pyportmgmt",
 	version="0.0.1",
 	author="Srinivasa Balaji",
 	author_email="rsbbalaji19@gmail.com",
